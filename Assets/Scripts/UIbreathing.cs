e7c489667fbac2bc62e3faf5093515843f3be800
zachary-coxsae <1026662@student.sae.edu.au>
2023-07-14 12:30:25 +1000

    1.7
    added ui and audio

---
+++ Assets/Scripts/UIbreathing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIbreathing : MonoBehaviour
+{
+    public Vector3 fullBreath = new Vector3(3f, 3f, 3f);
+    private void Start()
+    {
+        LeanTween.scale(gameObject, fullBreath, 3f).setEaseInCubic().setLoopPingPong();
+    }
+}
