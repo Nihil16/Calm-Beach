6fc2f9d523447dd4476edd5b3410b25a3b52ee66
zachary-coxsae <1026662@student.sae.edu.au>
2023-07-18 14:46:34 +1000

    v2.1

---
+++ Assets/Scripts/LunarRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LunarRotation : MonoBehaviour
+{
+    void Start()
+    {
+        LeanTween.rotate(gameObject, new Vector3(-16, 0, 0), 240);
+    }
+}
--- Assets/Scripts/UIbreathing.cs
+++ Assets/Scripts/UIbreathing.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 
 public class UIbreathing : MonoBehaviour
 {
-    public Vector3 fullBreath = new Vector3(3f, 3f, 3f);
+    [SerializeField] private Vector3 fullBreath = new Vector3(3f, 3f, 3f);
+
+    [SerializeField] private float timeTillTarget;
+
+    [Header ("Inhale")]
+    [SerializeField] private float StartInhaleTime = 1.8f;
+    [SerializeField] private float TargetInhaleTime = 3f;
+    [SerializeField] private float CurrentInhaleTime;
+
+    [Header("Hold")]
+    [SerializeField] private float StartHoldTime = 0.3f;
+    [SerializeField] private float TargetHoldTime = 0.6f;
+    [SerializeField] private float CurrentHoldTime;
+
+    [Header("Exhale")]
+    [SerializeField] private float StartExhaleTime = 4.2f;
+    [SerializeField] private float TargetExhaleTime = 7f;
+    [SerializeField] private float CurrentExhaleTime;
     private void Start()
     {
-        LeanTween.scale(gameObject, fullBreath, 3f).setEaseInCubic().setLoopPingPong();
+        LeanTween.value(StartInhaleTime, TargetInhaleTime, timeTillTarget).setOnUpdate((float value) => CurrentInhaleTime = value );
+        LeanTween.value(StartExhaleTime, TargetExhaleTime, timeTillTarget).setOnUpdate((float value) => CurrentExhaleTime = value );
+        LeanTween.value(StartHoldTime, TargetHoldTime, timeTillTarget).setOnUpdate((float value) => CurrentHoldTime = value );
+        StartInhale();
     }
+    private void StartInhale()
+    {
+        LeanTween.scale(gameObject, fullBreath, CurrentInhaleTime)
+            .setEaseInOutCubic()
+            .setOnComplete(() => StartExhale());
+    }
+    private void StartExhale()
+    {
+        LeanTween.scale(gameObject, Vector3.one, CurrentExhaleTime)
+            .setEaseInOutCubic()
+            .setOnComplete(() => StartInhale())
+            .setDelay(CurrentHoldTime);
+    }
+
+        
 }
