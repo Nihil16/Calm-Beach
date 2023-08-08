bb31bbe908aa5f3c7636c55dcb1860c7c646972a
zachary-coxsae <1026662@student.sae.edu.au>
2023-08-08 14:54:45 +1000

    5.2
    added game ending script

---
+++ Assets/GameEnder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnder : MonoBehaviour
+{
+    public AudioClip Narration;
+    public float clipLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clipLength = Narration.length;
+    }
+    void Update () 
+    {
+        clipLength -= Time.deltaTime;
+        if (clipLength <= 0 )
+        {
+            Application.Quit();
+        }
+    }
+    
+}
